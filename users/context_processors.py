29bc11e49609d8ce12071afb3269949581fcd571
Mstsislau <mr.chadway@gmail.com>
2023-12-28 13:05:20 +0300

    "some bugs were fixed"

--- users/context_processors.py
+++ users/context_processors.py
@@ -1,4 +1,3 @@
-# from news.utils import menu
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Обратная связь", 'url_name': 'contact'},
